e73153038a3e35a05b5bfb5d21bd0b7c5dbb2e7e
Yvonne M Pham <yvonnersz@gmail.com>
2020-08-07 08:06:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Code your solution here
 
 function findMatching(drivers, string) {
-  return drivers.filter(driver => driver === string)
+  return drivers.filter(driver => driver.toDowncase() === string.toDowncase())
 }
